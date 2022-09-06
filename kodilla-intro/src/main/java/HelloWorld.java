b691950ec80bf2a99421e141ffce478976384054
Slawek Kopytek <slavkopytek@gmail.com>
2022-09-06 21:03:44 +0200

    Hello world! with Java

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        // Generating primitive data types
+        System.out.println("Hello world");
+        System.out.println("Sławek");
+        System.out.println(27);
+        System.out.println('K');
+    }
+}
