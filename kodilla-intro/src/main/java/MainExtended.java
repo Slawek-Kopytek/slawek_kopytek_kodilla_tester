4010af7bff4cee104ca35a124daebc4f26a40692
Slawek Kopytek <slavkopytek@gmail.com>
2022-09-07 22:03:57 +0200

    Testing variable

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String carBrand = "Volkswagen";
+        System.out.println(carBrand);
+        String carModel= "Golf R mk7.5";
+        System.out.println(carModel);
+        double engineSize = 1.984;
+        System.out.println(engineSize);
+        String typeOfFuel = "petrol";
+        System.out.println(typeOfFuel);
+        int horsePower = 300;
+        System.out.println(horsePower);
+        char carClass = 'C';
+        System.out.println(carClass);
+        boolean hasAllWheelDrive = true;
+        System.out.println(hasAllWheelDrive);
+    }
+}
