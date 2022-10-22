d2de65d1740bb8731e06e1fe47b4a87bea73a8bb
Slawek Kopytek <slavkopytek@gmail.com>
2022-10-22 11:09:52 +0200

    3.6 Calculator JUnit

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subResult = calculator.subtract(a, b);
+        assertEquals(-3, subResult);
+    }
+
+    @Test
+    public void testPow() {
+        Calculator calculator = new Calculator();
+        int a = 3;
+        int b = 5;
+        double powResult = calculator.power(a, b);
+        assertEquals(243, powResult, 0.001);
+    }
+}
