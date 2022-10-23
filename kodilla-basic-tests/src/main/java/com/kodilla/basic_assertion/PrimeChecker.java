889eac9dee9bcb60c69cbe50f115bbcd1c9a693c
Slawek Kopytek <slavkopytek@gmail.com>
2022-10-23 20:27:17 +0200

    3.8 First tests Grades and Student

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/PrimeChecker.java
@@ -0,0 +1,25 @@
+package com.kodilla.basic_assertion;
+
+public class PrimeChecker {
+
+    private int count;
+
+    public boolean isPrime(int number) {
+
+        if(number < 2){
+            return false;
+        }
+        for (int i = 2; i < number; i++)
+            if (number % i == 0) {
+                return false;
+            }
+        return true;
+    }
+    public int getCount(){
+        return count;
+    }
+
+    public int incrementCount(){
+        return count++;
+    }
+}
