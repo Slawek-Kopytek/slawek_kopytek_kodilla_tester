1ef78256be7ffd494c314d18bef498a690fb6695
Slawek Kopytek <slavkopytek@gmail.com>
2023-01-20 00:21:53 +0100

    14.3 Wallet

---
+++ kodilla-cucumber/src/test/java/wallet/WalletSteps.java
@@ -0,0 +1,27 @@
+package wallet;
+
+import io.cucumber.java8.En;
+import org.junit.Assert;
+
+public class WalletSteps implements En {
+    private final Wallet wallet = new Wallet();
+    private final CashSlot cashSlot = new CashSlot();
+    private final Cashier cashier = new Cashier(cashSlot);
+
+
+    public WalletSteps() {
+
+        Given("I have deposited $200 in my wallet", () -> {
+            wallet.deposit(200);
+            Assert.assertEquals("Incorrect wallet balance", 200, wallet.getBalance());
+        });
+
+        When("I request $30", () -> {
+            cashier.withdraw(wallet, 30);
+        });
+
+        Then("$30 should be dispensed", () -> {
+            Assert.assertEquals(30, cashSlot.getContents());
+        });
+    }
+}
